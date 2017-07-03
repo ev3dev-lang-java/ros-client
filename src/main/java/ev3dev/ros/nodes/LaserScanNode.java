891f96759d7bb59665d008c97a7a4385c2627698
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-07-03 17:36:15 +0100

    Initial development

---
+++ src/main/java/ev3dev/ros/nodes/LaserScanNode.java
@@ -0,0 +1,43 @@
+package ev3dev.ros.nodes;
+
+import org.ros.concurrent.CancellableLoop;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.topic.Publisher;
+import sensor_msgs.LaserScan;
+import std_msgs.String;
+
+public class LaserScanNode extends AbstractNodeMain {
+
+    private final java.lang.String NODE_NAME = "BrickLaserScan";
+
+    @Override
+    public GraphName getDefaultNodeName() {
+        return GraphName.of(NODE_NAME);
+    }
+
+    @Override
+    public void onStart(ConnectedNode connectedNode) {
+
+        final Publisher<LaserScan> publisher =
+                connectedNode.newPublisher("scan", sensor_msgs.LaserScan._TYPE);
+
+        connectedNode.executeCancellableLoop(new CancellableLoop() {
+
+            @Override
+            protected void setup() {
+
+            }
+
+            @Override
+            protected void loop() throws InterruptedException {
+                sensor_msgs.LaserScan scan = publisher.newMessage();
+
+                publisher.publish(scan);
+                Thread.sleep(1000);
+            }
+
+        });
+    }
+}

52aad7bff44aaaacbf0596f7595256c17235aa9a
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-08-13 14:08:27 +0100

    Minor change

---
+++ src/main/java/ev3dev/ros/IMUPublisher.java
@@ -0,0 +1,46 @@
+package ev3dev.ros;
+
+import edu.wpi.rail.jrosbridge.Ros;
+import edu.wpi.rail.jrosbridge.Topic;
+import edu.wpi.rail.jrosbridge.messages.sensor_msgs.LaserScan;
+import edu.wpi.rail.jrosbridge.primitives.Time;
+import ev3dev.arduino.sensors.bn055.BNO055;
+import ev3dev.sensors.slamtec.RPLidarA1;
+import org.slf4j.Logger;
+
+/**
+ * Created by jabrena on 13/8/17.
+ */
+public class IMUPublisher {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(LaserScanPublisher.class);
+
+    private final Ros ros;
+    private final String topicName;
+    private final String DEFAULT_TOPIC_NAME = "imu";
+    private final String dataType = null;//Imu.TYPE;
+
+    private final String sensorPort;
+    private final String frameId;
+    final BNO055 bno055;
+    private Time previousTime;
+    private final Topic topic;
+    private int counter_seq = 0;
+
+    public IMUPublisher(
+            final Ros ros,
+            final String sensorPort,
+            final String frameId) {
+
+        this.ros = ros;
+        topicName = DEFAULT_TOPIC_NAME;
+        this.sensorPort = sensorPort;
+        this.frameId = frameId;
+        bno055 = new BNO055(sensorPort);
+
+        topic = new Topic(this.ros, this.topicName, dataType);
+
+    }
+
+
+}
